3479ce7879817771dde77bf08be3a53d7dee069f
Raakost <jeanettesydboege@gmail.com>
2017-02-19 20:53:17 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Course} from './Course';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Course Planner!';
+  /*courses = ['Course 1', 'Course 2', 'Course 3'];*/
+  courses: Course[];
+
+  constructor() {
+    this.courses = [{
+      academy: 'EASV',
+      year: 2017,
+      title: 'Angular2',
+      education: 'Computer Science',
+      lecturer: 'Lars Bilde'
+    },
+      {
+        academy: 'EASV',
+        year: 2017,
+        title: 'Mobile Apps',
+        education: 'Computer Science',
+        lecturer: 'Ole Eriksen'
+      }];
+  }
 }
---
+++ src/app/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+  academy?: string; //kan være null
+  year: number;
+  title: string;
+  education: string;
+  lecturer: string;
+}
